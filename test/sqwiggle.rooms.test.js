2e7e3bb37e7c9df9d4a469fe6247b9ca0cc94f3b
Will Barrett <will@tallgreentree.com>
2014-03-05 13:23:19 -0600

    Can now pull list of rooms

--- test/sqwiggle.rooms.test.js
+++ test/sqwiggle.rooms.test.js
@@ -1,13 +1,17 @@
 var should = require('should');
 var Sqwiggle = require('../index');
-var client = new Sqwiggle('token')
+var client = new Sqwiggle('cli_81fea39928d4d83558bff12ec80ed7fb')
 
-describe('Sqwiggle.rooms', function(){
-  it ('loads the list of rooms', function(done) {
-    client.rooms.list(function(err, response) {
-      console.log(err)
-      console.log(response)
-      done();
+describe('Rooms', function(){
+  describe('Sqwiggle.rooms.list') {
+    it ('loads the list of rooms', function(done) {
+      client.rooms.list(function(err, response) {
+        should.not.exist(err)
+        should.exist(response)
+        response[0].name.should.equal("Sqwiggle")
+        console.log('response: '+JSON.stringify(response))
+        done();
+      })
     })
-  })
-})
+  }
+});
