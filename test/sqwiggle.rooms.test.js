b40882af8e4e466434db410f44b81482def50361
Will Barrett <will@tallgreentree.com>
2014-03-05 13:52:53 -0600

    More detail on approach

--- test/sqwiggle.rooms.test.js
+++ test/sqwiggle.rooms.test.js
@@ -1,17 +1,27 @@
 var should = require('should');
+var nock = require('nock');
 var Sqwiggle = require('../index');
-var client = new Sqwiggle('cli_81fea39928d4d83558bff12ec80ed7fb')
+var client = new Sqwiggle('test_api_key')
 
 describe('Rooms', function(){
-  describe('Sqwiggle.rooms.list') {
+
+  describe('Sqwiggle.rooms.index', function() {
+    var scope = nock('https://api.sqwiggle.com')
+                    .get('/rooms')
+                    .reply(200, [{"id":1,"user_id":1,"name":"Sqwiggle","created_at":"2013-07-06T20:26:59Z","path":"","user_count":3},
+                      {"id":2,"user_id":2,"name":"Github","created_at":"2013-12-19T12:44:40Z","path":"github","user_count":0}
+                    ]);
+
     it ('loads the list of rooms', function(done) {
-      client.rooms.list(function(err, response) {
+      client.rooms.index(function(err, response) {
         should.not.exist(err)
         should.exist(response)
+        response.length.should.equal(2)
         response[0].name.should.equal("Sqwiggle")
-        console.log('response: '+JSON.stringify(response))
         done();
       })
     })
-  }
+
+  })
+
 });
